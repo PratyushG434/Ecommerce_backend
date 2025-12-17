c53c85deee55aeb68fb138088e1db4f06d95ad86
Anushka Singh <anushka.singh020307@gmail.com>
2025-12-17 12:47:35 +0530

    server.ts admin route fixed

--- src/server.ts
+++ src/server.ts
@@ -43,7 +43,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/admin', dashboardRoutes);
 app.use('/api/user', userRoutes); 
 app.use('/api/admin',adminRoutes);
-app.use('/api/admin',adminOnly, adminRoutes);
+app.use('/api/admin', adminRoutes);
 app.use('/api/payment', paymentRoutes);
 
 
